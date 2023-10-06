181c59546d54b49b897f25f45d9402538f5e21cf
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-10-06 15:09:30 +0200

    [add] randomEmail

--- script.js
+++ script.js
@@ -4,54 +4,25 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            // Dichiarazione di una variabile randomMail nell'oggetto data
+            randomMail: []
         }
     },
     methods: {
         getData() {
-            // Effettuo una richiesta HTTP GET a un endpoint API per ottenere dati casuali
+            for(let i = 0; i < 10 ; i++){
+              // Effettuo una richiesta HTTP GET a un endpoint API per ottenere dati casuali
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((mail)=>{
                 // Stampo la risposta della richiesta API nella console
                 console.log(mail.data.response);
+                 // Aggiunge il dato casuale all'array randomMail
+                 this.randomMail.push(mail.data.response);
 
             });
+            this.randomMail = [];  
+            }
+            
 
         }
     }
 }).mount('#app');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
