8d80369e130000aa248d85fb328fdfc1324e336b
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-10-06 14:53:37 +0200

    [add] stamp on console

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Vue Email List
 - Cro una struttura HTML e CSSscaffolding
 - Effettuo una richiesta HTTP GET a un endpoint API per ottenere dati casuali
+- Stampo la risposta della richiesta API nella console
--- script.js
+++ script.js
@@ -11,6 +11,8 @@ createApp({
         getData() {
             // Effettuo una richiesta HTTP GET a un endpoint API per ottenere dati casuali
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((mail)=>{
+                // Stampo la risposta della richiesta API nella console
+                console.log(mail.data.response);
 
             });
 
